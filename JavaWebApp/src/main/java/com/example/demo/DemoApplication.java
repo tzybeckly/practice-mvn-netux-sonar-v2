e4383cef1c74cf9c5c72ca0882763fdb5cf6a5af
beckly <nkezebeckly@gmail.com>
2022-10-18 19:41:54 -0400

    modified app code

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,12 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+		string name = "nkeze"; name = 'nkeze'
+
+		system.out.println(name);
+
+
 	}
 
 }
