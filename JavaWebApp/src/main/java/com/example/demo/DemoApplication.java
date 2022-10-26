0f334bd4bef0ad3012c62f948a70a2024d0eec67
beckly <nkezebeckly@gmail.com>
2022-10-25 21:13:00 -0400

    fix

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -9,11 +9,9 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
-		string name = "nkeze"; name = 'nkeze'
+		String name = "nkeze";
 
-		system.out.println(name);
-
-
+		System.out.println(name);
 	}
 
 }
